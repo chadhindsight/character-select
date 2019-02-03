dcee78711506dcd61822e195e75b77ab60181a0d
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-02-03 09:19:31 -0500

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+ const Footer = () => (
+     <div>
+         <footer>
+             <ul>
+                 <li><a>About us</a></li>
+                 <li><a>Blog</a></li>
+                 <li><a>Press</a></li>
+                 <li><a>iOS App</a></li>
+                 <li><a>Android App</a></li>
+             </ul>
+             <p>Copyrighted By BOOTY INC 2018</p>
+         </footer>
+     </div>
+ )
+ export default Footer;
