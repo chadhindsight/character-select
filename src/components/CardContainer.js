6532a4cee8bbcf2e20a8081537a71dd869793e7a
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-02-07 12:01:50 -0500

    Initial commit

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import CharacterCard from './CharacerCard.js';
 
-class CardContainer extends Component {
+class CardsContainer extends Component {
     constructor(props) {
         super(props)
     }
+    
     render() {
         return (
             <div>
@@ -12,4 +14,9 @@ class CardContainer extends Component {
         )
     }
 }
-export default CardContainer;
+export default CardsContainer;
+
+// Info for different characters will be stored in an array
+// From there you can import Character card component and pass info in to them as props
+// For each character in the array render a related Character card component
+
