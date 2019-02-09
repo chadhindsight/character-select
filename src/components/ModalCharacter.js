4bf7ce1c907d58fff98f60dcf9f38b4531d01898
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-02-09 12:53:02 -0500

    pass in props

--- src/components/ModalCharacter.js
+++ src/components/ModalCharacter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-const ModalCharacter = () => (
+const ModalCharacter = (props) => (
     <Modal
     // modal related props
     isOpen={}
