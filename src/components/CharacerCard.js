52632f990aaaef2a8193cbd238bf7eb7bd08ee40
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-02-10 14:11:14 -0500

    Add className

--- src/components/CharacerCard.js
+++ src/components/CharacerCard.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const CharacterCard = (props) => (
+    // Individual Character 
     <div>
         <h2>{}</h2>
-        
+        <p>{}</p>
+        <p>{}</p>
     </div>
 )
+export default CharacterCard
