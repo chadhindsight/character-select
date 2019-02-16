a93043f3a0538ff38c84b4721bc6e70a5aa6c342
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-02-16 13:33:40 -0500

    Populate characters array

--- src/store/characters.js
+++ src/store/characters.js
@@ -1,3 +1,15 @@
-const characters = [
+export const characters = [
+    {
+        name: '',
+        power: 505,
+        age: 23,
+        selected: false
+    },
+    {
+        name: '',
+        power: 353,
+        age: 30,
+        selected: false
+    },
 
-]
+] 
