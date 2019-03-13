034c8d0b47f6abaf5b8a1a331bf5c3f6b4723dfb
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-03-13 18:17:07 -0400

    Import CharacterCard and related info

--- src/components/ModalCharacter.js
+++ src/components/ModalCharacter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal';
+import CharacterCard from './CharacerCard';
 
 // This should pop when a character frame is clicked 
 const ModalCharacter = (props) => (
