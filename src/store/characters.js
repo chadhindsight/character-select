6985348b39307c4ee29a2a4f750957999850a118
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-03-16 18:03:46 -0400

    add id property to each character

--- src/store/characters.js
+++ src/store/characters.js
@@ -1,15 +1,30 @@
 export const characters = [
     {
-        name: '',
+        name: 'Mr Unkown',
         power: 505,
-        age: 23,
+        age: 45,
+        id: 0,
         selected: false
     },
     {
         name: 'Ashley',
         power: 353,
-        age: 30,
+        age: 19,
+        id: 1,
         selected: false
     },
-
+    {
+        name: 'Devin',
+        power: 453,
+        age: 30,
+        id: 2,
+        selected: false
+    },
+    {
+        name: 'Sorya',
+        power: 250,
+        age: 25,
+        id: 3,
+        selected: false
+    }
 ] 
