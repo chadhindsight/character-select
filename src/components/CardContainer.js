257dddb307a82450bf46eccd2211751463d070e6
Chad Hinds <chadrickj8@gmail.com>
2019-04-12 16:19:02 -0400

    for each character picture render a menu item

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -11,13 +11,18 @@ class CardsContainer extends Component {
     // Make picture list with .map. After doing this, delete this comment
     renderList() {
         this.props.characters.map( char => {
-            return <li></li>
+            return (
+                <li>
+                    <h2>{char.name}</h2>
+                    <img src={char.img}/> 
+                </li>
+            )
         })
     }
     render() {
         return (
             <div>
-                <ul></ul>
+                <ul>{this.renderList()}</ul>
             </div>
         )
     }
