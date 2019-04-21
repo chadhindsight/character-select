7d958a6912d9d7bcb16ad3bcc3fbad249cb304c3
Chad Hinds <chadrickj8@gmail.com>
2019-04-21 14:27:07 -0400

    create index that houses combineReducer

--- src/store/reducers/charsReducer.js
+++ src/store/reducers/charsReducer.js
@@ -2,10 +2,11 @@
 export default (state, action) => {
     switch (action.type) {
         case 'SELECT_CHARACTER':
-            
+            return action.payload
             break;
-    
+
         default:
+            return state
             break;
     }
 }
