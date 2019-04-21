08d55a8b11f4f9b0cd6d2af7e27a60e69a037f0a
Chad Hinds <chadrickj8@gmail.com>
2019-04-21 14:35:02 -0400

    Combine reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,3 +1,10 @@
 import { combineReducers } from 'redux';
 import charsReducer from './charsReducer';
 import characters from './characters';
+
+const rootReducer = combineReducers({
+    characters,
+    charsReducer
+})
+
+export default rootReducer
