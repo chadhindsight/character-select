68144303453019ef65a927b62cce8d4c619c0685
Chad Hinds <chadrickj8@gmail.com>
2019-04-23 19:56:17 -0400

    Add comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import rootReducer from './store/reducers';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
-// my life is garbage!
+const store = createStore(rootReducer);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'))
+// don't forget to add the Provider for redux later. Good to go!
